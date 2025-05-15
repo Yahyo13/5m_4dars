9d21b2cd39379cd10ed82dcdd65fe4eee6f408a9
Yahyo <mirxodjaevyahyo@gmail.com>
2025-05-15 17:06:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let a = prompt(`Ismingizni kiriting`)
+
+let b = prompt(`Xarfni kiriting`)
+
+if(a.includes(b) == true){
+    console.log(`bu xarf bor`);
+    
+}else{
+    console.log(`error`);
+    
+}
